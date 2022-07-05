03e67b77b5ef513334fd26e5d724bdbb31566e30
Cyril Zwahlen <zwahlen@precisionwave.com>
2022-07-05 14:21:27 +0200

    improved path dealing in install script

--- install-tools.sh
+++ install-tools.sh
@@ -1,11 +1,15 @@
-cd gpsd
+#!/bin/bash
+SCR_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+if [ "${SCR_PATH}" != "/root/tools" ]; then
+  echo "ERROR: install-tools.sh must be located in /root/tools/ !"
+  exit 1
+fi
+
+cd ${SCR_PATH}/gpsd
 ./install-gpsd.sh
-cd ..
 
-cd ntp
+cd ${SCR_PATH}/ntp
 ./install-ntp.sh
-cd ..
 
-cd gpsdo
+cd ${SCR_PATH}/gpsdo
 ./install-gpsdo.sh
-cd ..
